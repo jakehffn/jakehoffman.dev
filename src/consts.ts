f9d22b13df010273593c193fe8515277c7ee5542
Jake Hoffman <jaketh19@gmail.com>
2023-02-19 20:02:36 -0500

    Changed website description

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Jake Hoffman';
-export const SITE_DESCRIPTION = 'Jake Hoffman';
+export const SITE_DESCRIPTION = 'A Software Developer';
