a74a2cb15ba0936eb0f9c54827e12764e1ec1238
Jake Hoffman <jaketh19@gmail.com>
2023-05-01 14:49:44 -0400

    Updated colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,16 +9,37 @@ module.exports = {
 			xl: '1440px',
 		},
 		colors: {
-			'main-bg': '#151821',
-			'main-accent': '#ea3957',
+			// Original
+			// 'main-bg': '#151821',
+			// 'main-accent': '#ea3957',
+			// 'main-accent-hover': '#9d263a',
+			// 'main-text': '#c0c8c6',
+			// 'main-text-hover': '#767b7a',
+			// 'alt-accent': '#dd8970',
+			// 'main-subtle': '#454f6d',
+			// 'main-subtle-hover': '#2d3347',
+			// 'main-code-bg': '#282a36',
+
+			'main-bg': '#434A42',
+			'main-accent': '#586156',
 			'main-accent-hover': '#9d263a',
-			'main-text': '#c0c8c6',
-			'main-text-hover': '#767b7a',
-			'alt-accent': '#dd8970',
-			'main-subtle': '#454f6d',
-			'main-subtle-hover': '#2d3347',
-			'main-code-bg': '#282a36',
-			// 'footer-text'
+			'main-text': '#C4BBAF',
+			'main-text-hover': '#626C66',
+			'alt-accent': '#ACA885',
+			'main-subtle': '#586156',
+			'main-subtle-hover': '#626C66',
+			'main-code-bg': '#586156',
+ 
+			// Browns
+			// 'main-bg': '#5A2A27',
+			// 'main-accent': '#A5978B',
+			// 'main-accent-hover': '#5C4742',
+			// 'main-text': '#C4BBAF',
+			// 'main-text-hover': '#A5978B',
+			// 'alt-accent': '#8D5B4C',
+			// 'main-subtle': '#8D5B4C',
+			// 'main-subtle-hover': '#5C4742',
+			// 'main-code-bg': '#8D5B4C'
 		},
 		fontFamily: {
 			sans: ['Roboto', 'Helvetica', 'sans-serif'],
